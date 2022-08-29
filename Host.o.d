76e15bd79f9e46c1d04c2612ded1ec74b05ee69c
lixin-deng <83501683+lixin-deng@users.noreply.github.com>
2022-08-30 03:45:54 +0930

    1.0

---
+++ Host.o.d
@@ -0,0 +1,5 @@
+out/emcc-release//Host.o: Host.cc Host.h /root/omnetpp/include/omnetpp.h
+
+Host.h:
+
+/root/omnetpp/include/omnetpp.h:
---
+++ Server.o.d
@@ -0,0 +1,6 @@
+out/emcc-release//Server.o: Server.cc Server.h \
+  /root/omnetpp/include/omnetpp.h
+
+Server.h:
+
+/root/omnetpp/include/omnetpp.h:
